abdd2b97a4c5aa9af65b5b47f95e28c3fcc27aa8
abdellah oukim <abdellahoukim1999@gmail.com>
2023-01-29 19:57:48 +0100

    smal footerl modification

--- includes/footer.php
+++ includes/footer.php
@@ -15,7 +15,7 @@
             <li><a href="#">Facebook</a></li>
             <li><a href="#">Instagram</a></li>
             <li><a href="#">Twitter</a></li>
-            <li><a href="#">Whatsap</a></li>
+            <li><a href="#">Whatsapp</a></li>
           </ul>
         </div>
         <div class="links">
